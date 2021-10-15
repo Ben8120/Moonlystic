c70815123bfa2f1b0b1f73b3141d2be27e9494e8
Ben8120 <chuabenshern5@gmail.com>
2021-10-15 17:43:09 +0800

    added admin panels, codebehind for groupbutton

--- Moonlystic/Admin.aspx
+++ Moonlystic/Admin.aspx
@@ -1,5 +1,39 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Admin.aspx.cs" Inherits="Moonlystic.Admin" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
     <h1>Administrator Page</h1>
-    <p>Manage Category <br /> Manage Products <br /> Manage Users <br /> Manage FAQ <br /> Manage Home Page <br /> Manage Orders</p>
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                <div class="btn-group-vertical">
+                    <asp:Button ID="btnCategory" runat="server" Text="Category" class="btn btn-primary" OnClick="btnCategory_Click"/>
+                    <asp:Button ID="btnProducts" runat="server" Text="Products" class="btn btn-primary" OnClick="btnProducts_Click"/>
+                    <asp:Button ID="btnUsers" runat="server" Text="Users" class="btn btn-primary" OnClick="btnUsers_Click"/>
+                    <asp:Button ID="btnFAQ" runat="server" Text="FAQ" class="btn btn-primary" OnClick="btnFAQ_Click"/>
+                    <asp:Button ID="btnHome" runat="server" Text="Home Page" class="btn btn-primary" OnClick="btnHome_Click"/>
+                    <asp:Button ID="btnOrders" runat="server" Text="Orders" class="btn btn-primary" OnClick="btnOrders_Click"/>
+                </div>
+            </div>
+            <div class="col-8">
+                <asp:Panel ID="PanelCategory" runat="server">
+                    <p>Manage Category Panel</p>
+                </asp:Panel>
+                <asp:Panel ID="PanelManageProducts" runat="server">
+                    <p>Manage Products Panel</p>
+                </asp:Panel>
+                <asp:Panel ID="PanelManageUsers" runat="server">
+                    <p>Manage Users Panel</p>
+                </asp:Panel>
+                <asp:Panel ID="PanelManageFAQ" runat="server">
+                    <p>Manage FAQ Panel</p>
+                </asp:Panel>
+                <asp:Panel ID="PanelManageHome" runat="server">
+                    <p>Manage Home Page Panel</p>
+                </asp:Panel>
+                <asp:Panel ID="PanelManageOrders" runat="server">
+                    <p>Manage Orders Panel</p>
+                </asp:Panel>
+            </div>
+        </div>
+    </div>
 </asp:Content>
+
--- Moonlystic/Admin.aspx.cs
+++ Moonlystic/Admin.aspx.cs
@@ -9,9 +9,82 @@ namespace Moonlystic
 {
     public partial class Admin : System.Web.UI.Page
     {
+        string btnActive = "btn btn-primary active";
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                hideAllPanels();
+            }
+        }
 
+        protected void btnCategory_Click(object sender, EventArgs e)
+        {
+            toInactive();
+            btnCategory.Attributes["class"] = btnActive;
+            hideAllPanels();
+            PanelCategory.Visible = true;
+        }
+
+        protected void btnProducts_Click(object sender, EventArgs e)
+        {
+            toInactive();
+            btnProducts.Attributes["class"] = btnActive;
+            hideAllPanels();
+            PanelManageProducts.Visible = true;
+        }
+
+        protected void btnUsers_Click(object sender, EventArgs e)
+        {
+            toInactive();
+            btnUsers.Attributes["class"] = btnActive;
+            hideAllPanels();
+            PanelManageUsers.Visible = true;
+        }
+
+        protected void btnFAQ_Click(object sender, EventArgs e)
+        {
+            toInactive();
+            btnFAQ.Attributes["class"] = btnActive;
+            hideAllPanels();
+            PanelManageFAQ.Visible = true;
+        }
+
+        protected void btnHome_Click(object sender, EventArgs e)
+        {
+            toInactive();
+            btnHome.Attributes["class"] = btnActive;
+            hideAllPanels();
+            PanelManageHome.Visible = true;
+        }
+
+        protected void btnOrders_Click(object sender, EventArgs e)
+        {
+            toInactive();
+            btnOrders.Attributes["class"] = btnActive;
+            hideAllPanels();
+            PanelManageOrders.Visible = true;
+        }
+
+        protected void hideAllPanels()
+        {
+            PanelCategory.Visible = false;
+            PanelManageFAQ.Visible = false;
+            PanelManageHome.Visible = false;
+            PanelManageOrders.Visible = false;
+            PanelManageProducts.Visible = false;
+            PanelManageUsers.Visible = false;
+        }
+
+        protected void toInactive()
+        {
+            string inactive = "btn btn-primary";
+            btnCategory.Attributes["class"] = inactive;
+            btnFAQ.Attributes["class"] = inactive;
+            btnHome.Attributes["class"] = inactive;
+            btnOrders.Attributes["class"] = inactive;
+            btnProducts.Attributes["class"] = inactive;
+            btnUsers.Attributes["class"] = inactive;
         }
     }
 }
