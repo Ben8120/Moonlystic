d4605a4a2f71793b526053165b4b202426659d1d
Ben8120 <chuabenshern5@gmail.com>
2021-10-17 10:25:32 +0800

    updated category page to load from db category

--- Moonlystic/Category.aspx
+++ Moonlystic/Category.aspx
@@ -3,6 +3,14 @@
     <h1>Category</h1>
     <p>Horizontal Scrolling with Model Viewers inside</p>
     <div class="horizontal-scroll">
+        <% foreach (string catName in catNames) { %>
+        <div class="card h-scroll" style="width: 18rem;">
+            <img class="card-img-top" src="Assets/gray.jpg" alt="Card image cap">
+            <div class="card-body">
+                <p class="card-text"><% =catName %></p>
+            </div>
+        </div>
+        <% } %>
         <div class="card h-scroll" style="width: 18rem;">
             <img class="card-img-top" src="Assets/gray.jpg" alt="Card image cap">
             <div class="card-body">
--- Moonlystic/Category.aspx.cs
+++ Moonlystic/Category.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +11,36 @@ namespace Moonlystic
 {
     public partial class Category : System.Web.UI.Page
     {
+        public List<string> catNames = new List<string>();
         protected void Page_Load(object sender, EventArgs e)
         {
+            catNames = loadCatName();
+        }
 
+        protected List<String> loadCatName()
+        {
+            List<string> loadCatNames = new List<string>();
+
+            if (!IsPostBack)
+            {
+                string connStr = ConfigurationManager.ConnectionStrings["AvenueConnectionString"].ConnectionString;
+                SqlConnection conn = new SqlConnection(connStr);
+                conn.Open();
+
+                string sqlquery = "SELECT * FROM Category ";
+                SqlCommand comm = new SqlCommand(sqlquery, conn);
+
+                SqlDataReader reader = comm.ExecuteReader();
+                if (reader.Read() == true)
+                {
+                    loadCatNames.Add(reader["categoryName"].ToString());
+                } else
+                {
+                    //response.redirect
+                }
+            }
+
+            return loadCatNames;
         }
     }
 }
