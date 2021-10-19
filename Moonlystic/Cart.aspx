9fdb8159672ff6dfc0d4f792c91aa7138ad6b060
Ben8120 <chuabenshern5@gmail.com>
2021-10-19 14:31:56 +0800

    deferred cart db loading

---
+++ Moonlystic/Cart.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Cart.aspx.cs" Inherits="Moonlystic.Cart" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h1>Cart</h1>
+    <p>Cart page</p>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">o</th>
+                <th scope="col">Product Image</th>
+                <th scope="col">Product Name</th>
+                <th scope="col">Amount</th>
+                <th scope="col">Price</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="col">1</th>
+                <td><img src="Assets/gray.jpg" style="width:150px;height:150px;"/></td>
+                <td><p>Neko</p></td>
+                <td><p>1</p></td>
+                <td><p>RM10.00</p></td>
+            </tr>
+            <tr>
+                <th scope="col">2</th>
+                <td><img src="Assets/gray.jpg" style="width:150px;height:150px;"/></td>
+                <td><p>Catto</p></td>
+                <td><p>1</p></td>
+                <td><p>RM11.00</p></td>
+            </tr>
+            <tr>
+                <th scope="col">3</th>
+                <td colspan="3">Total:</td>
+                <td>RM21.00</td>
+            </tr>
+        </tbody>
+    </table>
+    <div class="row">
+        <div class="col-10">
+        </div>
+        <div class="col-2">
+            <asp:Button ID="btnBuy" runat="server" Text="Buy" class="btn btn-primary" OnClick="btnBuy_Click"/>
+        </div>
+    </div>
+</asp:Content>
---
+++ Moonlystic/Cart.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Moonlystic
+{
+    public partial class Cart : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["id"] != null)
+            {
+                //TODO: get product ID from each List
+            } else
+            {
+                Response.Redirect("SignIn.aspx");
+            }
+        }
+
+        protected List<List<int>> getCartItemData()
+        {
+            List<List<int>> cartIds = new List<List<int>>();
+
+            string connStr = ConfigurationManager.ConnectionStrings["AvenueConnectionString"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connStr);
+            conn.Open();
+
+            string sqlquery = "SELECT cartId, productId FROM Cart WHERE userId=@id";
+            SqlCommand comm = new SqlCommand(sqlquery, conn);
+            comm.Parameters.AddWithValue("@userId", Session["id"]);
+
+            SqlDataReader reader = comm.ExecuteReader();
+
+            while (reader.Read() == true)
+            {
+                cartIds.Add(new List<int> { int.Parse(reader["cartId"].ToString()), int.Parse(reader["productId"].ToString()) });
+            }
+
+            reader.Close();
+            conn.Close();
+
+            return cartIds;
+        }
+
+        protected void getCartDatas()
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["AvenueConnectionString"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connStr);
+            conn.Open();
+
+            string sqlquery = "SELECT * FROM Products WHERE productId=@productId";
+            SqlCommand comm = new SqlCommand(sqlquery, conn);
+            //comm.Parameters.AddWithValue("@userId", );
+        }
+
+        protected void btnBuy_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Payment.aspx");
+        }
+    }
+}
