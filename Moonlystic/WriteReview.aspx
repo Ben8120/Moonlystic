60e1f75f38de11e82efe14b2b9ef993fa36df870
Ben8120 <chuabenshern5@gmail.com>
2021-10-27 14:47:49 +0800

    added review function to unreviewed products

--- Moonlystic/WriteReview.aspx
+++ Moonlystic/WriteReview.aspx
@@ -3,12 +3,12 @@
     <div class="vh-100 row">
         <div class="card w-100 align-self-center">
             <h1>Review Page</h1>
-            <h3>Product Name</h3>
+            <h3><% =productName %></h3>
             <p>Your Review:</p>
             <div class="input-group mb-3">
                 <asp:TextBox ID="txtReview" runat="server" class="form-control w-100" aria-describedby="btnReview" TextMode="MultiLine"></asp:TextBox>
                 <div class="input-group-append">
-                    <asp:Button ID="btnReview" runat="server" Text="Review" class="btn-btn-outline-secondary" />
+                    <asp:Button ID="btnReview" runat="server" Text="Review" class="btn-btn-outline-secondary" OnClick="btnReview_Click" />
                 </div>
             </div>
         </div>
--- Moonlystic/WriteReview.aspx.cs
+++ Moonlystic/WriteReview.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +11,95 @@ namespace Moonlystic
 {
     public partial class WriteReview : System.Web.UI.Page
     {
+        protected static int cartId;
+        protected string productName;
+        protected bool isEmpty;
         protected void Page_Load(object sender, EventArgs e)
         {
+            cartId = 0;
+            cartId = Convert.ToInt16(Request.QueryString["cartId"]);
+            if (cartId == 0)
+            {
+                Response.Redirect("Profile.aspx");
+            } else
+            {
+                isEmpty = checknull();
+                if (isEmpty)
+                {
+                    getProductName();
+                } else
+                {
+                    Response.Redirect("Profile.aspx");
+                }
+            }
 
+        }
+
+        protected void getProductName()
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["AvenueConnectionString"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connStr);
+            conn.Open();
+
+            string sqlquery = "SELECT Product.productName FROM Cart INNER JOIN Product ON Cart.productId = Product.productId WHERE Cart.cartId = @id";
+            SqlCommand comm = new SqlCommand(sqlquery, conn);
+            comm.Parameters.AddWithValue("@id", cartId);
+
+            SqlDataReader reader = comm.ExecuteReader();
+            if (reader.Read() == true)
+            {
+                productName = reader["productName"].ToString();
+            }
+            reader.Close();
+            conn.Close();
+        }
+
+        protected void btnReview_Click(object sender, EventArgs e)
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["AvenueConnectionString"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connStr);
+            conn.Open();
+
+            string sqlQuery = "UPDATE Cart SET orderReview=@orderReview WHERE cartId=@cartId";
+            SqlCommand command = new SqlCommand(sqlQuery, conn);
+            command.Parameters.AddWithValue("@orderReview", txtReview.Text);
+            command.Parameters.AddWithValue("@cartId", cartId);
+
+            command.ExecuteNonQuery();
+            conn.Close();
+
+            Response.Redirect("Default.aspx");
+        }
+
+        protected bool checknull()
+        {
+            bool isEmpty = false;
+            string strReview;
+
+            string connStr = ConfigurationManager.ConnectionStrings["AvenueConnectionString"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connStr);
+            conn.Open();
+
+            string sqlquery = "SELECT orderReview FROM Cart WHERE cartId=@id";
+            SqlCommand comm = new SqlCommand(sqlquery, conn);
+            comm.Parameters.AddWithValue("@id", cartId);
+
+            SqlDataReader reader = comm.ExecuteReader();
+            if (reader.Read() == true)
+            {
+                strReview = reader["orderReview"].ToString();
+                if (strReview == "")
+                {
+                    isEmpty = true;
+                } else
+                {
+                    isEmpty = false;
+                }
+            }
+            reader.Close();
+            conn.Close();
+
+            return isEmpty;
         }
     }
 }
